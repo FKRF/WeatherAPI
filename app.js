b24f14ba7a5f68559f9426ba9e5f52439aa541f9
FKRF <153143103+FKRF@users.noreply.github.com>
2025-09-04 15:49:04 -0300

    address-autocomplete

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var apiKey = "c16065cf71f35ce65d3c93abd80d3d5b";
 var apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
-var FozDoIguaçu = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Foz do Iguaçu&appid=c16065cf71f35ce65d3c93abd80d3d5b" 
+// var FozDoIguaçu = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=Foz do Iguaçu&appid=c16065cf71f35ce65d3c93abd80d3d5b" 
 
-var searchBox = document.querySelector(".search input");
-var searchBtn = document.querySelector(".search button");
+var searchBox = document.querySelector(".search-autocomplete input");
+var searchBtn = document.querySelector(".search-autocomplete button");
+
+const input = document.querySelector("#city");
 const weatherIcon = document.querySelector(".weather-icon");
+
+let cities = [];
+
+async function carregarCidades() {
+    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/municipios`;
+    const response = await fetch(url);
+    cities = await response.json();
+}
+
+carregarCidades();
+
+function searchCities(query)
+{
+    return cities.filter(c => c.nome.toLowerCase().includes(query)).slice(0, 5);
+    // return cidades.filter(c => c.nome.toLowerCase().includes(query.toLowerCase())).slice(0, 10); // limitar resultados
+}
+
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
@@ -37,6 +56,41 @@ async function checkWeather(city) {
     
 }
 
+input.addEventListener("input", async () => {
+    let query = input.value.trim().toLowerCase();
+    if (query.length < 3)
+        return;
+
+    let results = searchCities(query);
+    showSuggestions(results);
+})
+
+function showSuggestions(cities)
+{
+    const list = document.querySelector("#suggestions");
+    list.innerHTML = "";
+
+    cities.forEach(city => {
+        let li = document.createElement("li");
+        li.textContent = `${city.nome} / ${city.microrregiao.mesorregiao.UF.sigla}`;
+        li.addEventListener("click", () => {
+            input.value = city.name;
+            list.innerHTML = "";
+        })
+        list.appendChild(li);
+
+    })
+}
+
 searchBtn.addEventListener("click", ()=> {
     checkWeather(searchBox.value);
 })
+
+searchBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter")
+    {
+        checkWeather(searchBox.value);
+    }
+})
+
+
